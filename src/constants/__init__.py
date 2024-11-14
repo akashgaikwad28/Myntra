37ac93dd13e6a729f2a02bd1131e64133360a311
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2024-11-14 21:00:57 +0530

    "Added app.py with Streamlit UI for Myntra review scrapper and constants/__init__.py with MongoDB URL and database name"

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://acashtech28:<db_password>@cluster0.wz402.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
